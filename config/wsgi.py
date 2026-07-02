4580ffe07b3bda62f501f90b124925e2696d1782
Sumit000018521 <sumitchavan5774@gmail.com>
2026-07-02 12:38:50 -0400

    Temporary create superuser

--- config/wsgi.py
+++ config/wsgi.py
@@ -14,3 +14,17 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
+
+from django.contrib.auth import get_user_model
+
+try:
+    User = get_user_model()
+    if not User.objects.filter(username="chavanpheta01").exists():
+        User.objects.create_superuser(
+            username="chavanpheta01",
+            email="chavanpheta01@gmail.com",
+            password="chavanpheta01@"
+        )
+except Exception:
+    pass
